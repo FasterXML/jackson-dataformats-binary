f35321992afb8bae2533bc02196aed33d8153b46
Tatu Saloranta <tatu.saloranta@iki.fi>
2016-06-27 13:37:11 -0700

    Fix #24

--- cbor/src/main/java/com/fasterxml/jackson/dataformat/cbor/CBORParser.java
+++ cbor/src/main/java/com/fasterxml/jackson/dataformat/cbor/CBORParser.java
@@ -817,7 +817,7 @@ public final class CBORParser extends ParserMinimalBase
         if (neg) {
             i = -i - 1;
         }
-        return String.valueOf(1);
+        return String.valueOf(i);
     }
 
     protected JsonToken _handleTaggedBinary(int tag) throws IOException
