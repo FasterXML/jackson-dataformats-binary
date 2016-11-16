4fa051f08dbd06348bb56fa036cd1c8d0af81cc0
Tatu Saloranta <tatu.saloranta@iki.fi>
2016-11-16 10:44:55 -0800

    add a failing test to verify future fix

---
+++ cbor/src/test/java/com/fasterxml/jackson/dataformat/cbor/failing/NumberCoercing32Test.java
@@ -0,0 +1,34 @@
+package com.fasterxml.jackson.dataformat.cbor.failing;
+
+import java.util.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.fasterxml.jackson.dataformat.cbor.*;
+
+public class NumberCoercing32Test extends CBORTestBase
+{
+    /*
+    /**********************************************************
+    /* Test methods
+    /**********************************************************
+     */
+
+    public void testPrimitiveTypeInvariance() throws Exception
+    {
+        ObjectMapper mapper = new ObjectMapper(
+                new CBORFactory().disable(CBORGenerator.Feature.WRITE_MINIMAL_INTS)
+                );
+        Map<String, Object> map = new HashMap<>();
+        map.put("longField", 1L);
+        map.put("intField", 1);
+        map.put("doubleField", 1.0d);
+        map.put("floatField", 1.0f);
+        byte[] json = mapper.writeValueAsBytes(map);
+        Map<String, Object> result = mapper.readerFor(Map.class).readValue(json);
+        assertEquals(Integer.class, result.get("intField").getClass());
+        assertEquals(Long.class, result.get("longField").getClass());
+        assertEquals(Double.class, result.get("doubleField").getClass());
+        assertEquals(Float.class, result.get("floatField").getClass());
+    }
+}
