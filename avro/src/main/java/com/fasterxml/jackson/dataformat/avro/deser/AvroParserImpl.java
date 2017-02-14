cb1ae17fdd62a42c576ae848907b5cb1ae9d9398
Tatu Saloranta <tatu.saloranta@iki.fi>
2017-02-13 23:13:39 -0800

    clean up merge

--- avro/src/main/java/com/fasterxml/jackson/dataformat/avro/deser/AvroParserImpl.java
+++ avro/src/main/java/com/fasterxml/jackson/dataformat/avro/deser/AvroParserImpl.java
@@ -2,7 +2,6 @@ package com.fasterxml.jackson.dataformat.avro.deser;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.ByteBuffer;
 
 import org.apache.avro.io.BinaryDecoder;
 
--- avro/src/main/java/com/fasterxml/jackson/dataformat/avro/deser/StructDefaults.java
+++ avro/src/main/java/com/fasterxml/jackson/dataformat/avro/deser/StructDefaults.java
@@ -2,8 +2,6 @@ package com.fasterxml.jackson.dataformat.avro.deser;
 
 import java.io.IOException;
 
-import org.apache.avro.io.BinaryDecoder;
-
 import com.fasterxml.jackson.core.JsonToken;
 
 public class StructDefaults
@@ -11,8 +9,7 @@ public class StructDefaults
     public static AvroFieldReader createObjectDefaults(String name,
             String[] fieldNames, AvroStructureReader[] fieldReaders) {
         return AvroFieldReader.construct(name, new ObjectDefaults(
-                null, null, null,
-                fieldNames, fieldReaders));
+                null, null, fieldNames, fieldReaders));
     }
 
     protected static class ObjectDefaults extends MapReader
@@ -20,20 +17,17 @@ public class StructDefaults
         protected final String[] _fieldNames;
         protected final AvroStructureReader[] _fieldReaders;
 
-        public ObjectDefaults(AvroReadContext parent,
-                AvroParserImpl parser, BinaryDecoder decoder,
+        public ObjectDefaults(AvroReadContext parent, AvroParserImpl parser,
                 String[] fieldNames, AvroStructureReader[] fieldReaders)
         {
-            super(parent, parser, decoder);
+            super(parent, parser);
             _fieldNames = fieldNames;
             _fieldReaders = fieldReaders;
         }
 
         @Override
-        public MapReader newReader(AvroReadContext parent,
-                AvroParserImpl parser, BinaryDecoder decoder) {
-            return new ObjectDefaults(parent, parser, decoder,
-                    _fieldNames, _fieldReaders);
+        public MapReader newReader(AvroReadContext parent, AvroParserImpl parser) {
+            return new ObjectDefaults(parent, parser, _fieldNames, _fieldReaders);
         }
 
         @Override
@@ -51,20 +45,12 @@ public class StructDefaults
                     return (_currToken = JsonToken.FIELD_NAME);
                 }
                 final AvroReadContext parent = getParent();
-                // as per [dataformats-binary#38], may need to reset, instead of bailing out
-                if (parent.inRoot()) {
-                    if (!DecodeUtil.isEnd(_decoder)) {
-                        _index = 0;
-                        _state = STATE_START;
-                        return (_currToken = JsonToken.END_OBJECT);
-                    }
-                }
                 _state = STATE_DONE;
                 _parser.setAvroContext(parent);
                 return (_currToken = JsonToken.END_OBJECT);
             case STATE_VALUE:
                 _state = STATE_NAME;
-                AvroStructureReader r = _fieldReaders[_index].newReader(this, _parser, _decoder);
+                AvroStructureReader r = _fieldReaders[_index].newReader(this, _parser);
                 _parser.setAvroContext(r);
                 ++_index;
                 return (_currToken = r.nextToken());
@@ -75,7 +61,7 @@ public class StructDefaults
         }
 
         @Override
-        public void skipValue(BinaryDecoder decoder) throws IOException {
+        public void skipValue(AvroParserImpl parser) throws IOException {
             // never called defaults
         }
     }
