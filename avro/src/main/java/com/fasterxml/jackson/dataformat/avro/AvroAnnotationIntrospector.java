623b004f80d811baaf0b5c83fb0f1f40774782dc
Bryan Harclerode <baharclerode@gmail.com>
2017-02-18 17:26:25 -0600

    [Avro] Add support for @AvroName annotation

--- avro/src/main/java/com/fasterxml/jackson/dataformat/avro/AvroAnnotationIntrospector.java
+++ avro/src/main/java/com/fasterxml/jackson/dataformat/avro/AvroAnnotationIntrospector.java
@@ -1,13 +1,18 @@
 package com.fasterxml.jackson.dataformat.avro;
 
+import com.fasterxml.jackson.databind.PropertyName;
 import com.fasterxml.jackson.databind.introspect.Annotated;
 import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
 import org.apache.avro.reflect.AvroIgnore;
+import org.apache.avro.reflect.AvroName;
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.annotate.JsonProperty;
 
 /**
  * Adds support for the following annotations from the Apache Avro implementation:
  * <ul>
- * <li>{@link AvroIgnore @AvroIgnore} - Alias for {@code @JsonIgnore(true)}</li>
+ * <li>{@link AvroIgnore @AvroIgnore} - Alias for {@link JsonIgnore @JsonIgnore(true)}</li>
+ * <li>{@link AvroName @AvroName("custom Name")} - Alias for {@link JsonProperty @JsonProperty("custom name")}</li>
  * </ul>
  */
 public class AvroAnnotationIntrospector extends JacksonAnnotationIntrospector {
@@ -15,5 +20,21 @@ public class AvroAnnotationIntrospector extends JacksonAnnotationIntrospector {
     @Override
     protected boolean _isIgnorable(Annotated a) {
         return a.getAnnotation(AvroIgnore.class) != null || super._isIgnorable(a);
+    }
+
+    @Override
+    public PropertyName findNameForSerialization(Annotated a) {
+        if (a.hasAnnotation(AvroName.class)) {
+            return PropertyName.construct(a.getAnnotation(AvroName.class).value());
+        }
+        return super.findNameForSerialization(a);
+    }
+
+    @Override
+    public PropertyName findNameForDeserialization(Annotated a) {
+        if (a.hasAnnotation(AvroName.class)) {
+            return PropertyName.construct(a.getAnnotation(AvroName.class).value());
+        }
+        return super.findNameForDeserialization(a);
     }
 }
