4fbad2900e788a56271ac072cd142024f2f8123d
Tatu Saloranta <tatu.saloranta@iki.fi>
2017-04-14 18:53:11 -0700

    change zigzag for protobuf codec too

--- protobuf/src/main/java/com/fasterxml/jackson/dataformat/protobuf/ProtobufUtil.java
+++ protobuf/src/main/java/com/fasterxml/jackson/dataformat/protobuf/ProtobufUtil.java
@@ -92,41 +92,18 @@ public class ProtobufUtil
     }
     
     public static int zigzagEncode(int input) {
-        // Canonical version:
-        //return (input << 1) ^  (input >> 31);
-        // but this is even better
-        if (input < 0) {
-            return (input << 1) ^ -1;
-        }
-        return (input << 1);
+        return (input << 1) ^  (input >> 31);
     }
 
     public static int zigzagDecode(int encoded) {
-        // canonical:
-        //return (encoded >>> 1) ^ (-(encoded & 1));
-        if ((encoded & 1) == 0) { // positive
-            return (encoded >>> 1);
-        }
-        // negative
-        return (encoded >>> 1) ^ -1;
+        return (encoded >>> 1) ^ (-(encoded & 1));
     }
-    
+
     public static long zigzagEncode(long input) {
-        // Canonical version
-        //return (input << 1) ^  (input >> 63);
-        if (input < 0L) {
-            return (input << 1) ^ -1L;
-        }
-        return (input << 1);
+        return (input << 1) ^  (input >> 63);
     }
 
     public static long zigzagDecode(long encoded) {
-        // canonical:
-        //return (encoded >>> 1) ^ (-(encoded & 1));
-        if ((encoded & 1) == 0) { // positive
-            return (encoded >>> 1);
-        }
-        // negative
-        return (encoded >>> 1) ^ -1L;
+        return (encoded >>> 1) ^ (-(encoded & 1));
     }
 }
