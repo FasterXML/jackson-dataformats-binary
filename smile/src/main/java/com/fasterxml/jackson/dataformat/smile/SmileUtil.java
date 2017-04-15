2b4f77b46024c482195bd712523067b912755cf0
Tatu Saloranta <tatu.saloranta@iki.fi>
2017-04-14 18:48:52 -0700

    minor perf improvement for zigzag encode/decode for smile, to avoid branching

--- smile/src/main/java/com/fasterxml/jackson/dataformat/smile/SmileUtil.java
+++ smile/src/main/java/com/fasterxml/jackson/dataformat/smile/SmileUtil.java
@@ -6,41 +6,46 @@ package com.fasterxml.jackson.dataformat.smile;
 public class SmileUtil
 {
     public static int zigzagEncode(int input) {
-        // Canonical version:
-        //return (input << 1) ^  (input >> 31);
-        // but this is even better
+        // canonical, avoids branching, which matters more than alu ops
+        return (input << 1) ^  (input >> 31);
+        /*
         if (input < 0) {
             return (input << 1) ^ -1;
         }
         return (input << 1);
+        */
     }
 
     public static int zigzagDecode(int encoded) {
-        // canonical:
-        //return (encoded >>> 1) ^ (-(encoded & 1));
-        if ((encoded & 1) == 0) { // positive
+        // canonical, avoids branching
+        return (encoded >>> 1) ^ (-(encoded & 1));
+        /*
+        if ((encoded & 1) == 0) {
             return (encoded >>> 1);
         }
-        // negative
         return (encoded >>> 1) ^ -1;
+        */
     }
     
     public static long zigzagEncode(long input) {
         // Canonical version
-        //return (input << 1) ^  (input >> 63);
+        return (input << 1) ^  (input >> 63);
+        /*
         if (input < 0L) {
             return (input << 1) ^ -1L;
         }
         return (input << 1);
+        */
     }
 
     public static long zigzagDecode(long encoded) {
         // canonical:
-        //return (encoded >>> 1) ^ (-(encoded & 1));
-        if ((encoded & 1) == 0) { // positive
+        return (encoded >>> 1) ^ (-(encoded & 1));
+        /*
+        if ((encoded & 1) == 0) {
             return (encoded >>> 1);
         }
-        // negative
         return (encoded >>> 1) ^ -1L;
+        */
     }
 }
