b9e001c0964c4d47d1cf6204dd795bf9d4a567b5
Kenji Noguchi <tokyo246@gmail.com>
2017-05-13 19:19:40 -0700

    add complete test

--- protobuf/src/test/java/com/fasterxml/jackson/dataformat/protobuf/schema/DescriptorLoaderCompleteTest.java
+++ protobuf/src/test/java/com/fasterxml/jackson/dataformat/protobuf/schema/DescriptorLoaderCompleteTest.java
@@ -1,8 +1,27 @@
 package com.fasterxml.jackson.dataformat.protobuf.schema;
 
-/**
- * Created by user on 5/13/17.
- */
-public class DescriptorLoaderCompleteTest
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.dataformat.protobuf.ProtobufFactory;
+import com.fasterxml.jackson.dataformat.protobuf.ProtobufTestBase;
+
+import java.io.InputStream;
+import java.io.StringReader;
+
+public class DescriptorLoaderCompleteTest extends ProtobufTestBase
 {
+
+    private final ObjectMapper MAPPER = new ObjectMapper(new ProtobufFactory());
+
+
+    public void testParsing() throws Exception
+    {
+        // load unittest.desc descriptor file.
+        // This file was created at the top directory of Protobuf project.
+        // protoc - o unittest_proto3.desc google/protobuf/unittest_proto3.proto
+        InputStream in = this.getClass().getResourceAsStream("/unittest_proto3.desc");
+        FileDescriptorSet fds = DescriptorLoader.std.load(in);
+        ProtobufSchema schema2 = fds.forType("TestAllTypes");
+        assertNotNull(schema2);
+    }
 }
