d3b8fb963a16d2fb2934173cd6b817240055fe6e
Marcos Passos <marcos@marcospassos.com>
2019-08-06 16:26:16 -0300

    Fix union serialization

--- avro/src/main/java/com/fasterxml/jackson/dataformat/avro/ser/AvroWriteContext.java
+++ avro/src/main/java/com/fasterxml/jackson/dataformat/avro/ser/AvroWriteContext.java
@@ -287,7 +287,7 @@ public abstract class AvroWriteContext
             //  - ByteBuffer
             //  - Number wrappers (Integer, ...)
             
-            /*
+
             String typeId = AvroSchemaHelper.getTypeId(datum.getClass());
             for (int i = 0, size = types.size(); i < size; ++i) {
                 Schema schema = types.get(i);
@@ -296,7 +296,6 @@ public abstract class AvroWriteContext
                     return i;
                 }
             }
-            */
         }
 //System.err.println("Missing index for: "+datum.getClass().getName()+" ("+types.size()+") ->\n"+types);  
         return ReflectData.get().resolveUnion(unionSchema, datum);
@@ -338,7 +337,7 @@ public abstract class AvroWriteContext
                 return types.get(_resolveMapIndex(unionSchema, types, datum));
             }
 
-            /*
+
             String typeId = AvroSchemaHelper.getTypeId(datum.getClass());
             for (int i = 0, size = types.size(); i < size; ++i) {
                 Schema schema = types.get(i);
@@ -347,7 +346,6 @@ public abstract class AvroWriteContext
                     return schema;
                 }
             }
-            */
         }
 //System.err.println("Missing schema for: "+datum.getClass().getName()+" ("+types.size()+") ->\n"+types);  
         int ix = ReflectData.get().resolveUnion(unionSchema, datum);
