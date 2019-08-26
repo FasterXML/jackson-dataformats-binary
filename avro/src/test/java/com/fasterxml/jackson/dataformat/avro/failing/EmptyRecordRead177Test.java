fc55b4d55b211f6c85e4e10541cf3504678a263c
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-08-26 16:01:19 -0700

    Streamline test a bit

--- avro/src/test/java/com/fasterxml/jackson/dataformat/avro/failing/EmptyRecordRead177Test.java
+++ avro/src/test/java/com/fasterxml/jackson/dataformat/avro/failing/EmptyRecordRead177Test.java
@@ -1,8 +1,5 @@
 package com.fasterxml.jackson.dataformat.avro.failing;
 
-import org.apache.avro.Schema;
-import org.apache.avro.SchemaBuilder;
-
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 
 import com.fasterxml.jackson.dataformat.avro.AvroMapper;
@@ -13,11 +10,8 @@ public class EmptyRecordRead177Test extends AvroTestBase
 {
     private final AvroMapper MAPPER = getMapper();
 
-    private Schema SCHEMA = SchemaBuilder
-            .builder(EmptyRecordRead177Test.class.getName() + "$")
-            .record(Empty.class.getSimpleName())
-                .fields()
-            .endRecord();
+    private final AvroSchema SCHEMA = parseSchema(MAPPER,
+"{'type':'record', 'name':'Empty','namespace':'something','fields':[]}");
 
     @JsonAutoDetect // just a marker to avoid "no properties found" exception
     static final class Empty {
@@ -29,11 +23,11 @@ public class EmptyRecordRead177Test extends AvroTestBase
 
     public void testEmptyRecord() throws Exception {
         final Empty empty = new Empty();
-        
-        byte[] ser = MAPPER.writer().with(new AvroSchema(SCHEMA)).writeValueAsBytes(empty);
+
+        byte[] ser = MAPPER.writer().with(SCHEMA).writeValueAsBytes(empty);
 
         final Empty result = MAPPER.readerFor(Empty.class)
-                .with(new AvroSchema(SCHEMA))
+                .with(SCHEMA)
                 .readValue(ser);
 
         assertEquals(empty, result);
