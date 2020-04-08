ae1b4f43d5ca2378f1ca2356959b0725403fd64d
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-04-08 09:44:57 -0700

    More testing, some refactoring wrt #201

--- cbor/src/test/java/com/fasterxml/jackson/dataformat/cbor/gen/ArrayGenerationTest.java
+++ cbor/src/test/java/com/fasterxml/jackson/dataformat/cbor/gen/ArrayGenerationTest.java
@@ -31,7 +31,69 @@ public class ArrayGenerationTest extends CBORTestBase
         _testDoubleArray();
     }
 
-    public void testMinimalIntValues2() throws Exception
+    public void testMinimalIntValuesForInt() throws Exception
+    {
+        // Array with 3 values, with different sizing
+        _testMinimalIntValuesForInt(1, -1, 3, 11); // single-byte
+        _testMinimalIntValuesForInt(200, -200, 5, 11); // two-byte (marker, 0xFF)
+        _testMinimalIntValuesForInt(0xC831, -50000, 7, 11); // three-byte (marker, 0xFFFF)
+        _testMinimalIntValuesForInt(0x35690001, -(0x7FFFFFF0), 11, 11); // full
+    }
+
+    private void _testMinimalIntValuesForInt(int v1, int v2,
+            int minLen, int fullLen) throws Exception
+    {
+        final int[] input = new int[] { v1, v2 };
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        CBORGenerator gen = FACTORY.createGenerator(bytes);
+        assertTrue(gen.isEnabled(CBORGenerator.Feature.WRITE_MINIMAL_INTS));
+        gen.writeArray(input, 0, 2);
+        gen.close();
+
+        // With default settings, should get:
+        byte[] encoded = bytes.toByteArray();
+        assertEquals(minLen, encoded.length);
+
+        // then verify contents
+
+        CBORParser p = FACTORY.createParser(encoded);
+        assertToken(JsonToken.START_ARRAY, p.nextToken());
+        assertToken(JsonToken.VALUE_NUMBER_INT, p.nextToken());
+        assertEquals(NumberType.INT, p.getNumberType());
+        assertEquals(input[0], p.getIntValue());
+        assertToken(JsonToken.VALUE_NUMBER_INT, p.nextToken());
+        assertEquals(NumberType.INT, p.getNumberType());
+        assertEquals(input[1], p.getIntValue());
+        assertToken(JsonToken.END_ARRAY, p.nextToken());
+        p.close();
+
+        // but then also check without minimization
+        bytes = new ByteArrayOutputStream();
+        gen = FACTORY.createGenerator(bytes);
+        gen.disable(CBORGenerator.Feature.WRITE_MINIMAL_INTS);
+
+        gen.writeArray(input, 0, 2);
+        gen.close();
+
+        // With default settings, should get:
+        encoded = bytes.toByteArray();
+        assertEquals(fullLen, encoded.length);
+
+        // then verify contents
+
+        p = FACTORY.createParser(encoded);
+        assertToken(JsonToken.START_ARRAY, p.nextToken());
+        assertToken(JsonToken.VALUE_NUMBER_INT, p.nextToken());
+        assertEquals(NumberType.INT, p.getNumberType());
+        assertEquals(input[0], p.getIntValue());
+        assertToken(JsonToken.VALUE_NUMBER_INT, p.nextToken());
+        assertEquals(NumberType.INT, p.getNumberType());
+        assertEquals(input[1], p.getIntValue());
+        assertToken(JsonToken.END_ARRAY, p.nextToken());
+        p.close();
+    }
+    
+    public void testMinimalIntValuesForLong() throws Exception
     {
         // Array with 2 values that can't be passed as `int`s but DO fit
         // CBOR 5-byte int (sign + 32-bits)
@@ -63,6 +125,29 @@ public class ArrayGenerationTest extends CBORTestBase
         p.close();
 
         // but then also check without minimization
+        bytes = new ByteArrayOutputStream();
+        gen = FACTORY.createGenerator(bytes);
+        gen.disable(CBORGenerator.Feature.WRITE_MINIMAL_INTS);
+
+        gen.writeArray(input, 0, 2);
+        gen.close();
+
+        // With default settings, should get:
+        encoded = bytes.toByteArray();
+        assertEquals(19, encoded.length);
+
+        // then verify contents
+
+        p = FACTORY.createParser(encoded);
+        assertToken(JsonToken.START_ARRAY, p.nextToken());
+        assertToken(JsonToken.VALUE_NUMBER_INT, p.nextToken());
+        assertEquals(NumberType.LONG, p.getNumberType());
+        assertEquals(input[0], p.getLongValue());
+        assertToken(JsonToken.VALUE_NUMBER_INT, p.nextToken());
+        assertEquals(NumberType.LONG, p.getNumberType());
+        assertEquals(input[1], p.getLongValue());
+        assertToken(JsonToken.END_ARRAY, p.nextToken());
+        p.close();
     }
 
     private void _testIntArray() throws Exception {
