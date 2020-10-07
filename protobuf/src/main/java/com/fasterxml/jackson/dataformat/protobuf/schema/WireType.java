7bb6bfcd7e0ad058a967b9b8f85dc5985b83fb64
Elif Nur Çevik <elifcevik48@gmail.com>
2020-10-07 12:56:28 +0300

    Deleted Redundant Interface Modifier

--- protobuf/src/main/java/com/fasterxml/jackson/dataformat/protobuf/schema/WireType.java
+++ protobuf/src/main/java/com/fasterxml/jackson/dataformat/protobuf/schema/WireType.java
@@ -5,10 +5,10 @@ package com.fasterxml.jackson.dataformat.protobuf.schema;
  */
 public interface WireType
 {
-    public final static int VINT = 0;
-    public final static int FIXED_64BIT = 1;
-    public final static int LENGTH_PREFIXED = 2;
-    public final static int GROUP_START = 3;
-    public final static int GROUP_END = 4;
-    public final static int FIXED_32BIT = 5;
+    int VINT = 0;
+    int FIXED_64BIT = 1;
+    int LENGTH_PREFIXED = 2;
+    int GROUP_START = 3;
+    int GROUP_END = 4;
+    int FIXED_32BIT = 5;
 }
