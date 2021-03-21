0a26e237249d895a99947110ff314aaca6f616c2
Tatu Saloranta <tatu.saloranta@iki.fi>
2021-03-20 18:03:53 -0700

    Try to use a "big switch" for SmileParser.nextToken(), see if any speedup

--- smile/src/main/java/com/fasterxml/jackson/dataformat/smile/SmileParser.java
+++ smile/src/main/java/com/fasterxml/jackson/dataformat/smile/SmileParser.java
@@ -383,123 +383,170 @@ public class SmileParser extends SmileParserBase
             	return _eofAsNextToken();
             }
         }
-        int ch = _inputBuffer[_inputPtr++] & 0xFF;
+        final int ch = _inputBuffer[_inputPtr++] & 0xFF;
         _typeAsInt = ch;
-        switch (ch >> 5) {
-        case 0: // short shared string value reference
-            if (ch != 0) { // 0x0 is invalid
-                return _handleSharedString(ch-1);
-            }
-            break;
 
-        case 1: // simple literals, numbers
-            {
-                int typeBits = ch & 0x1F;
-                if (typeBits < 4) {
-                    switch (typeBits) {
-                    case 0x00:
-                        _textBuffer.resetWithEmpty();
-                        return (_currToken = JsonToken.VALUE_STRING);
-                    case 0x01:
-                        return (_currToken = JsonToken.VALUE_NULL);
-                    case 0x02: // false
-                        return (_currToken = JsonToken.VALUE_FALSE);
-                    default: // 0x03 == true
-                        return (_currToken = JsonToken.VALUE_TRUE);
-                    }
-                }
-                if (typeBits == 4) {
-                    _finishInt();
-                    return (_currToken = JsonToken.VALUE_NUMBER_INT);
-                }
-                // next 3 bytes define subtype
-                if (typeBits <= 6) { // VInt (zigzag), BigInteger
-                    _tokenIncomplete = true;
-                    return (_currToken = JsonToken.VALUE_NUMBER_INT);
-                }
-                if (typeBits < 11 && typeBits != 7) { // floating-point
-                    _tokenIncomplete = true;
-                    return (_currToken = JsonToken.VALUE_NUMBER_FLOAT);
-                }
-                if (typeBits == 0x1A) { // == 0x3A == ':' -> possibly header signature for next chunk?
-                    if (handleSignature(false, false)) {
-                        /* Ok, now; end-marker and header both imply doc boundary and a
-                         * 'null token'; but if both are seen, they are collapsed.
-                         * We can check this by looking at current token; if it's null,
-                         * need to get non-null token
-                         */
-                        if (_currToken == null) {
-                            return nextToken();
-                        }
-                        return (_currToken = null);
-                    }
-                    _reportError("Unrecognized token byte 0x3A (malformed segment header?");
-            	}
-            }
-            // and everything else is reserved, for now
-            break;
-        case 2: // tiny ASCII
-            // fall through            
-        case 3: // short ASCII
-            // fall through
-        case 4: // tiny Unicode
-            // fall through
-        case 5: // short Unicode
-            // No need to decode, unless we have to keep track of back-references (for shared string values)
-            if (_seenStringValueCount >= 0) { // shared text values enabled
-                return _addSeenStringValue();
-            }
-            _tokenIncomplete = true;
+        switch (ch) {
+        // MAJOR 0: short shared string value reference
+
+        case 0x0: // except for this value
+            return _reportUnknownTypeToken(ch);
+        case 0x1: case 0x2: case 0x3: case 0x4: case 0x5: case 0x6: case 0x7:
+        case 0x8: case 0x9: case 0xA: case 0xB: case 0xC: case 0xD: case 0xE: case 0xF:
+        case 0x10: case 0x11: case 0x12: case 0x13: case 0x14: case 0x15: case 0x16: case 0x17:
+        case 0x18: case 0x19: case 0x1A: case 0x1B: case 0x1C: case 0x1D: case 0x1E: case 0x1F:
+            return _handleSharedString(ch-1);
+
+        // MAJOR 1: simple literals, numbers
+
+        case 0x20: // 
+            _textBuffer.resetWithEmpty();
             return (_currToken = JsonToken.VALUE_STRING);
-        case 6: // small integers; zigzag encoded
+        case 0x21:
+            return (_currToken = JsonToken.VALUE_NULL);
+        case 0x22: // false
+            return (_currToken = JsonToken.VALUE_FALSE);
+        case 0x23: // 0x03 == true
+            return (_currToken = JsonToken.VALUE_TRUE);
+        case 0x24:
+            _finishInt();
+            return (_currToken = JsonToken.VALUE_NUMBER_INT);
+        case 0x25: case 0x26: // VInt (zigzag), BigInteger
+            _tokenIncomplete = true;
+            return (_currToken = JsonToken.VALUE_NUMBER_INT);
+        case 0x27: // reserved for future use
+            return _reportUnknownTypeToken(ch);
+        case 0x28: case 0x29: case 0x2A: // floating-point
+            _tokenIncomplete = true;
+            return (_currToken = JsonToken.VALUE_NUMBER_FLOAT);
+        case 0x2B: case 0x2C: case 0x2D: case 0x2E: case 0x2F:
+        case 0x30: case 0x31: case 0x32: case 0x33: case 0x34: case 0x35: case 0x36: case 0x37:
+        case 0x38: case 0x39: // reserved for future use
+            return _reportUnknownTypeToken(ch);
+        case 0x3A:
+        {
+            // == 0x3A == ':' -> possibly header signature for next chunk?
+            if (handleSignature(false, false)) {
+                // Ok, now; end-marker and header both imply doc boundary and a
+                // 'null token'; but if both are seen, they are collapsed.
+                // We can check this by looking at current token; if it's null,
+                // need to get non-null token
+                if (_currToken == null) {
+                    return nextToken();
+                }
+                return (_currToken = null);
+            }
+            _reportError("Unrecognized token byte 0x3A (malformed segment header?");
+        }
+        
+        // MAJOR 2: tiny ASCII
+        // MAJOR 3: short ASCII
+        // MAJOR 4: tiny Unicode
+        // MAJOR 5 short Unicode
+
+        // not included here, outside switch
+
+        /*
+        case 0x40: case 0x41: case 0x42: case 0x43: case 0x44: case 0x45: case 0x46: case 0x47:
+        case 0x48: case 0x49: case 0x4A: case 0x4B: case 0x4C: case 0x4D: case 0x4E: case 0x4F:
+        case 0x50: case 0x51: case 0x52: case 0x53: case 0x54: case 0x55: case 0x56: case 0x57:
+        case 0x58: case 0x59: case 0x5A: case 0x5B: case 0x5C: case 0x5D: case 0x5E: case 0x5F:
+
+        case 0x60: case 0x61: case 0x62: case 0x63: case 0x64: case 0x65: case 0x66: case 0x67:
+        case 0x68: case 0x69: case 0x6A: case 0x6B: case 0x6C: case 0x6D: case 0x6E: case 0x6F:
+        case 0x70: case 0x71: case 0x72: case 0x73: case 0x74: case 0x75: case 0x76: case 0x77:
+        case 0x78: case 0x79: case 0x7A: case 0x7B: case 0x7C: case 0x7D: case 0x7E: case 0x7F:
+
+        case 0x80: case 0x81: case 0x82: case 0x83: case 0x84: case 0x85: case 0x86: case 0x87:
+        case 0x88: case 0x89: case 0x8A: case 0x8B: case 0x8C: case 0x8D: case 0x8E: case 0x8F:
+        case 0x90: case 0x91: case 0x92: case 0x93: case 0x94: case 0x95: case 0x96: case 0x97:
+        case 0x98: case 0x99: case 0x9A: case 0x9B: case 0x9C: case 0x9D: case 0x9E: case 0x9F:
+            // fall through
+
+        case 0xA0: case 0xA1: case 0xA2: case 0xA3: case 0xA4: case 0xA5: case 0xA6: case 0xA7:
+        case 0xA8: case 0xA9: case 0xAA: case 0xAB: case 0xAC: case 0xAD: case 0xAE: case 0xAF:
+        case 0xB0: case 0xB1: case 0xB2: case 0xB3: case 0xB4: case 0xB5: case 0xB6: case 0xB7:
+        case 0xB8: case 0xB9: case 0xBA: case 0xBB: case 0xBC: case 0xBD: case 0xBE: case 0xBF:
+        */
+
+        // MAJOR 6: small integers; zigzag encoded
+        case 0xC0: case 0xC1: case 0xC2: case 0xC3: case 0xC4: case 0xC5: case 0xC6: case 0xC7:
+        case 0xC8: case 0xC9: case 0xCA: case 0xCB: case 0xCC: case 0xCD: case 0xCE: case 0xCF:
+        case 0xD0: case 0xD1: case 0xD2: case 0xD3: case 0xD4: case 0xD5: case 0xD6: case 0xD7:
+        case 0xD8: case 0xD9: case 0xDA: case 0xDB: case 0xDC: case 0xDD: case 0xDE: case 0xDF:
             _numberInt = SmileUtil.zigzagDecode(ch & 0x1F);
             _numTypesValid = NR_INT;
             _numberType = NumberType.INT;
             return (_currToken = JsonToken.VALUE_NUMBER_INT);
-        case 7: // binary/long-text/long-shared/start-end-markers
-            switch (ch & 0x1F) {
-            case 0x00: // long variable length ASCII
-            case 0x04: // long variable length unicode
-                _tokenIncomplete = true;
-                return (_currToken = JsonToken.VALUE_STRING);
-            case 0x08: // binary, 7-bit (0xE8)
-                _tokenIncomplete = true;
-                return (_currToken = JsonToken.VALUE_EMBEDDED_OBJECT);
-            case 0x0C: // long shared string (0xEC)
-            case 0x0D:
-            case 0x0E:
-            case 0x0F:
-                if (_inputPtr >= _inputEnd) {
-                    _loadMoreGuaranteed();
-                }
-                return _handleSharedString(((ch & 0x3) << 8) + (_inputBuffer[_inputPtr++] & 0xFF));
-            case 0x18: // START_ARRAY
-                _streamReadContext = _streamReadContext.createChildArrayContext(-1, -1);
-                return (_currToken = JsonToken.START_ARRAY);
-            case 0x19: // END_ARRAY
-                if (!_streamReadContext.inArray()) {
-                    _reportMismatchedEndMarker(']', '}');
-                }
-                _streamReadContext = _streamReadContext.getParent();
-                return (_currToken = JsonToken.END_ARRAY);
-            case 0x1A: // START_OBJECT
-                _streamReadContext = _streamReadContext.createChildObjectContext(-1, -1);
-                return (_currToken = JsonToken.START_OBJECT);
-            case 0x1B: // not used in this mode; would be END_OBJECT
-                _reportError("Invalid type marker byte 0xFB in value mode (would be END_OBJECT in key mode)");
-            case 0x1D: // binary, raw
-                _tokenIncomplete = true;
-                return (_currToken = JsonToken.VALUE_EMBEDDED_OBJECT);
-            case 0x1F: // 0xFF, end of content
-                return (_currToken = null);
+
+        // MAJOR 7: binary/long-text/long-shared/start-end-markers
+        case 0xE0: // long variable length ASCII
+            _tokenIncomplete = true;
+            return (_currToken = JsonToken.VALUE_STRING);
+        case 0xE1: case 0xE2: case 0xE3: // reserved for future use
+            return _reportUnknownTypeToken(ch);
+        case 0xE4: // long variable length unicode
+            _tokenIncomplete = true;
+            return (_currToken = JsonToken.VALUE_STRING);
+        case 0xE5: case 0xE6: case 0xE7: // reserved for future use
+            return _reportUnknownTypeToken(ch);
+        case 0xE8: // binary, 7-bit (0xE8)
+            _tokenIncomplete = true;
+            return (_currToken = JsonToken.VALUE_EMBEDDED_OBJECT);
+        case 0xE9: case 0xEA: case 0xEB: // reserved for future use
+            return _reportUnknownTypeToken(ch);
+        case 0xEC: // long shared string (0xEC)
+        case 0xED:
+        case 0xEE:
+        case 0xEF:
+            if (_inputPtr >= _inputEnd) {
+                _loadMoreGuaranteed();
             }
-            break;
+            return _handleSharedString(((ch & 0x3) << 8) + (_inputBuffer[_inputPtr++] & 0xFF));
+        case 0xF0: case 0xF1: case 0xF2: case 0xF3: // Reserved for future use
+        case 0xF4: case 0xF5: case 0xF6: case 0xF7: // Reserved for future use
+            return _reportUnknownTypeToken(ch);
+        case 0xF8: // START_ARRAY
+            _streamReadContext = _streamReadContext.createChildArrayContext(-1, -1);
+            return (_currToken = JsonToken.START_ARRAY);
+        case 0xF9: // END_ARRAY
+            if (!_streamReadContext.inArray()) {
+                _reportMismatchedEndMarker(']', '}');
+            }
+            _streamReadContext = _streamReadContext.getParent();
+            return (_currToken = JsonToken.END_ARRAY);
+        case 0xFA: // START_OBJECT
+            _streamReadContext = _streamReadContext.createChildObjectContext(-1, -1);
+            return (_currToken = JsonToken.START_OBJECT);
+        case 0xFB: // not used in this mode; would be END_OBJECT
+            _reportError("Invalid type marker byte 0xFB in value mode (would be END_OBJECT in key mode)");
+        case 0xFC: // end-of-String marker -- not used here
+            return _reportUnknownTypeToken(ch);
+        case 0xFD: // binary, raw
+            _tokenIncomplete = true;
+            return (_currToken = JsonToken.VALUE_EMBEDDED_OBJECT);
+        case 0xFE: // reserved for future use
+            return _reportUnknownTypeToken(ch);
+        case 0xFF: // 0xFF, end of content
+            return (_currToken = null);
         }
-        // If we get this far, type byte is corrupt
+        
+        // And now short text values:
+
+        // No need to decode, unless we have to keep track of back-references (for shared string values)
+        if (_seenStringValueCount >= 0) { // shared text values enabled
+            return _addSeenStringValue();
+        }
+        _tokenIncomplete = true;
+        return (_currToken = JsonToken.VALUE_STRING);
+    }
+
+    // should we change description based on reserved vs illegal?
+    private JsonToken _reportUnknownTypeToken(int ch) throws IOException {
         _reportError("Invalid type marker byte 0x"+Integer.toHexString(ch & 0xFF)+" for expected value token");
         return null;
     }
-
+    
     private final JsonToken _handleSharedString(int index) throws IOException
     {
         if (index >= _seenStringValueCount) {
