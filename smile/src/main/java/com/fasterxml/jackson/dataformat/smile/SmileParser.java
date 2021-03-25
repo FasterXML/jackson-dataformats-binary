7229150e6297ad7f33fdce709447160748feec9d
Tatu Saloranta <tatu.saloranta@iki.fi>
2021-03-24 18:01:12 -0700

    Fix a small problem wrt #265 fix (via backported 3.0 test)

--- smile/src/main/java/com/fasterxml/jackson/dataformat/smile/SmileParser.java
+++ smile/src/main/java/com/fasterxml/jackson/dataformat/smile/SmileParser.java
@@ -2642,6 +2642,7 @@ currentToken(), firstCh);
                     bb.write(buffer, 0, bufPtr);
                     bufPtr = 0;
                 }
+                left -= 7;
             }
 
             // And then the last one; we know there is room in buffer so:
@@ -2657,7 +2658,7 @@ currentToken(), firstCh);
                 }
                 // last byte is different, has remaining 1 - 6 bits, right-aligned
                 value <<= left;
-                buffer[bufPtr] = (byte) (value + _inputBuffer[_inputPtr++]);
+                buffer[bufPtr++] = (byte) (value + _inputBuffer[_inputPtr++]);
             }
             if (bufPtr > 0) {
                 bb.write(buffer, 0, bufPtr);
