2bba4e64adb99eedcc0bfe6c09acd8310b4cc308
Tatu Saloranta <tatu.saloranta@iki.fi>
2021-03-27 11:25:31 -0700

    Add failing test case for #267 (likely address only in 2.13)

--- cbor/src/test/java/com/fasterxml/jackson/dataformat/cbor/CBORTestBase.java
+++ cbor/src/test/java/com/fasterxml/jackson/dataformat/cbor/CBORTestBase.java
@@ -278,4 +278,25 @@ public abstract class CBORTestBase
         }
         return bout.toByteArray();
     }
+
+    protected byte[] readResource(String ref)
+    {
+       ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+       final byte[] buf = new byte[4000];
+
+       try (InputStream in = getClass().getResourceAsStream(ref)) {
+           if (in != null) {
+               int len;
+               while ((len = in.read(buf)) > 0) {
+                   bytes.write(buf, 0, len);
+               }
+           }
+       } catch (IOException e) {
+           throw new RuntimeException("Failed to read resource '"+ref+"': "+e);
+       }
+       if (bytes.size() == 0) {
+           throw new IllegalArgumentException("Failed to read resource '"+ref+"': empty resource?");
+       }
+       return bytes.toByteArray();
+    }
 }
