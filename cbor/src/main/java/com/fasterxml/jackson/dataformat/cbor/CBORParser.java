492dca9d12c4f714518f5cf2a6265cf6a1b6cee3
PJ Fanning <pjfanning@users.noreply.github.com>
2023-04-07 01:43:42 +0200

    validate big decimal scale before converting to big-int (#371)

--- cbor/src/main/java/com/fasterxml/jackson/dataformat/cbor/CBORParser.java
+++ cbor/src/main/java/com/fasterxml/jackson/dataformat/cbor/CBORParser.java
@@ -2346,6 +2346,7 @@ public class CBORParser extends ParserMinimalBase
     {
         if ((_numTypesValid & NR_BIGDECIMAL) != 0) {
             // here it'll just get truncated, no exceptions thrown
+            streamReadConstraints().validateBigIntegerScale(_numberBigDecimal.scale());
             _numberBigInt = _numberBigDecimal.toBigInteger();
         } else if ((_numTypesValid & NR_LONG) != 0) {
             _numberBigInt = BigInteger.valueOf(_numberLong);
