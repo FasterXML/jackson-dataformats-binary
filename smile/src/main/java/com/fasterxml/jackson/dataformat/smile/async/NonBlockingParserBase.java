0e28539bb2fcb3219138c6857cdb0877d720a8bc
Carter Kozak <ckozak@ckozak.net>
2023-05-11 12:54:23 -0400

    core#1015 non-blocking smile parser respects CANONICALIZE_FIELD_NAMES (#378)

--- smile/src/main/java/com/fasterxml/jackson/dataformat/smile/async/NonBlockingParserBase.java
+++ smile/src/main/java/com/fasterxml/jackson/dataformat/smile/async/NonBlockingParserBase.java
@@ -531,6 +531,10 @@ public abstract class NonBlockingParserBase
 
     protected final String _addDecodedToSymbols(int len, String name) throws IOException
     {
+        // 5-May-2023, ckozak: [core#1015] respect CANONICALIZE_FIELD_NAMES factory config.
+        if (!_symbolsCanonical) {
+            return name;
+        }
         if (len < 5) {
             return _symbols.addName(name, _quad1);
         }
