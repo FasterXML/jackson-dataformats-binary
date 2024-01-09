a5af432e4e2c398f8e1df7244e24a710a857b1db
Tatu Saloranta <tatu.saloranta@iki.fi>
2024-01-08 21:35:13 -0800

    Change detection of corrupt length indicator

--- cbor/src/test/java/com/fasterxml/jackson/dataformat/cbor/fuzz/CBORFuzz451_65617_IOOBETest.java
+++ cbor/src/test/java/com/fasterxml/jackson/dataformat/cbor/fuzz/CBORFuzz451_65617_IOOBETest.java
@@ -1,13 +1,14 @@
 package com.fasterxml.jackson.dataformat.cbor.fuzz;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.core.exc.StreamReadException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.fasterxml.jackson.dataformat.cbor.CBORTestBase;
 
-public class Fuzz451_65617_IOOBETest extends CBORTestBase
+public class CBORFuzz451_65617_IOOBETest extends CBORTestBase
 {
     private final ObjectMapper MAPPER = cborMapper();
 
@@ -16,7 +17,8 @@ public class Fuzz451_65617_IOOBETest extends CBORTestBase
         final byte[] input = readResource("/data/clusterfuzz-cbor-65617.cbor");
         try (JsonParser p = MAPPER.createParser(input)) {
             try {
-                p.nextToken();
+                assertToken(JsonToken.VALUE_STRING, p.nextToken());
+                // Important: do not access String, force skipping
                 p.nextToken();
                 fail("Should not reach here (invalid input)");
             } catch (StreamReadException e) {
