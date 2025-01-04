61bdbff7875f80a9804f88e55dd1da0086bd3799
Tatu Saloranta <tatu.saloranta@iki.fi>
2025-01-03 18:24:09 -0800

    Add more Smile tests, extend fix

--- smile/src/main/java/com/fasterxml/jackson/dataformat/smile/SmileParser.java
+++ smile/src/main/java/com/fasterxml/jackson/dataformat/smile/SmileParser.java
@@ -1186,6 +1186,9 @@ versionBits));
     public String getValueAsString(String defaultValue) throws IOException
     {
         if (_currToken != JsonToken.VALUE_STRING) {
+            if (_currToken == JsonToken.FIELD_NAME) {
+                return currentName();
+            }
             if (_currToken == null || _currToken == JsonToken.VALUE_NULL || !_currToken.isScalarValue()) {
                 return defaultValue;
             }
