5b636290974274d47ba193c9299890d5b5ea2999
Manuel Sugawara (∩｀-´)⊃━炎炎炎炎炎 <sugmanue@amazon.com>
2025-12-19 13:15:01 -0800

    Fix a out of bounds exception when writing to the outBuf

--- cbor/src/main/java/tools/jackson/dataformat/cbor/CBORParser.java
+++ cbor/src/main/java/tools/jackson/dataformat/cbor/CBORParser.java
@@ -2592,7 +2592,7 @@ public class CBORParser extends ParserBase
         int outPtr = 0;
         while (len > 0) {
             // load as much input as possible
-            int size = Math.min(len, Math.min(outBuf.length, input.length));
+            int size = Math.min(len, Math.min((outBuf.length - outPtr), input.length));
             if (!_tryToLoadToHaveAtLeast(size)) {
                 return len;
             }
